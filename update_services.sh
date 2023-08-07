57187ec4b93e6bbbd6450f63646e8e8a5133da1a
takez0_o <takez0_o@protonmail.com>
2023-08-07 16:51:13 +0300

    bash

--- update_services.sh
+++ update_services.sh
@@ -2,6 +2,8 @@
 
 git pull
 echo "Pulled latest changes from git"
+make build
+echo "Built app"
 cd app && docker build -f app.dockerfile -t batudal/de-risk-app:0.0.1 . && docker push batudal/de-risk-app:0.0.1
 echo "Built and pushed app image"
 docker service update --image batudal/de-risk-app:0.0.1 app_de-risk
